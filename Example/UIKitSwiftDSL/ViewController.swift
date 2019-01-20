7f12b154518edffb1757853b3a1e0dc873d81910
Majid Jabrayilov <cmecid@gmail.com>
2019-01-20 14:19:56 +0300

    Example code added.

--- Example/UIKitSwiftDSL/ViewController.swift
+++ Example/UIKitSwiftDSL/ViewController.swift
@@ -6,19 +6,42 @@
 //  Copyright (c) 2019 Majid Jabrayilov. All rights reserved.
 //
 
-import UIKit
+import UIKitSwiftDSL
 
 class ViewController: UIViewController {
+    override func loadView() {
+        view = stack {
+            $0.axis = .vertical
+            $0.isLayoutMarginsRelativeArrangement = true
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            $0.stack {
+                $0.axis = .horizontal
+
+                $0.label {
+                    $0.textColor = .white
+                    $0.text = "Hello"
+                }
+
+                $0.label {
+                    $0.textColor = .white
+                    $0.text = "World"
+                }
+
+                $0.label {
+                    $0.textColor = .white
+                    $0.text = "!!!"
+                }
+            }
+
+            $0.button {
+                $0.tintColor = .white
+                $0.setTitle("Say Hi!", for: .normal)
+            }
+
+            $0.view {
+                $0.backgroundColor = .clear
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
